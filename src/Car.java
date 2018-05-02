17b79fd1c41d2e8cb67e6e85c7d9ea782978da23
Claudio Luedi <luedic@ict-campus.net>
2018-05-02 14:12:49 +0200

    Claudio 1.2

--- src/Car.java
+++ src/Car.java
@@ -7,48 +7,42 @@ import java.nio.file.Path;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class Car extends JPanel{
+public class Car extends JPanel {
 	private Image sprite;
 	private int locX;
 	private int locY;
 	private int width = 56;
 	private int height = 24;
-	
-	
-	
-	public Car(int locX,int locY) {
+
+	public Car(int locX, int locY) {
 		this.locX = locX;
 		this.locY = locY;
 	}
+
 	public void loadImage() {
-        
-        ImageIcon i = new ImageIcon("images/mercedes.png");
-        sprite = i.getImage(); 
-        width = sprite.getWidth(null);
-        height = sprite.getHeight(null);
-    }
+
+	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
 		g.setColor(Color.RED);
 		g.drawRect(locX, locY, width, height);
-		g.drawImage(sprite ,locX, locY, null);
+		ImageIcon i = new ImageIcon("images/mercedes.png");
+		sprite = i.getImage();
+		g.drawImage(sprite, locX, locY, null);
 	}
 
 	public int getLocX() {
 		return locX;
 	}
 
-
 	public void setLocX(int locX) {
 		this.locX = locX;
 	}
 
-
 	public int getLocY() {
 		return locY;
 	}
-
 
 	public void setLocY(int locY) {
 		this.locY = locY;
